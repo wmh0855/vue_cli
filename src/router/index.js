3e5c70a04b2f934d0fc0685867f8f7f4933d60ac
wenmenghao <wmh0855@163.com>
2020-08-01 23:09:05 +0800

    添加常用代码

--- src/router/index.js
+++ src/router/index.js
@@ -14,5 +14,10 @@ const routes = [
 const router = new VueRouter({
   routes
 });
-
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title
+  }
+  next()
+})
 export default router;
